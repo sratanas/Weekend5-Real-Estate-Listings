21ac2ffd7719909676225818f275e20efc2aee66
Stephanie Ratanas <stephanieratanas@gmail.com>
2017-12-12 10:41:54 -0600

    updated port

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use('/rent', rent);
 app.use('/sale', sale);
 
 /** ---------- START SERVER ---------- **/
-var port = 5000;
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
     console.log('Listening on port: ', port);
 });
